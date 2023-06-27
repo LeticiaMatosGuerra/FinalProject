340d8fb7015bd6a8bfec66906a6c997e2fc140a3
Jacques Ehrardt <ehrardt38@gmail.com>
2023-06-27 10:42:10 -0700

    Add HeaderComponent template

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,11 @@
+<template>
+   <header>
+      <a href="./" class="logo">
+         <img src="../assets/Dish_dig_logo.png" alt="Dish Dig Logo">
+      </a>
+      <a href="./" class="profile">
+         <p>Hello, {{user.name}}</p>
+         <img :src="user.pic" alt="Profile Pic">
+      </a>
+   </header>
+</template>
