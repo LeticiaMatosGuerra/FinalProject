98848dd480011282823d4abc54567f5e0e2999ec
Jacques Ehrardt <ehrardt38@gmail.com>
2023-06-27 13:47:32 -0700

    Add DishConponent

---
+++ src/components/DishComponent.vue
@@ -0,0 +1,89 @@
+<template>
+   <main>
+      <figure>
+         <img src="../assets/logo.png" alt="dish">
+      </figure>
+      <section class="dishInfo">
+         <h1>Dish Name</h1>
+         <ul>
+            <li>
+               <i class="fa-solid fa-clock"></i>
+               <article>
+                  <h4>Prep Time</h4>
+                  <p># Minutes</p>
+               </article>
+            </li>
+            <li>
+               <i class="fa-solid fa-clock"></i>
+               <article>
+                  <h4>Cook Time</h4>
+                  <p># Minutes</p>
+               </article>
+            </li>
+            <li>
+               <i class="fa-solid fa-users"></i>
+               <article>
+                  <p>Serves #</p>
+               </article>
+            </li>
+         </ul>
+      </section>
+      <section>
+         <figure>
+            <img src="../assets/logo.png" alt="dish">
+         </figure>
+         <article>
+            <h3>Nutrition Information</h3>
+            <ul>
+               <li>
+                  <p>Lorem ipsum dolor sit amet.</p>
+               </li>
+               <li>
+                  <p>Aliquid animi voluptatem modi error.</p>
+               </li>
+               <li>
+                  <p>Magni blanditiis quae officiis veritatis.</p>
+               </li>
+            </ul>
+         </article>
+      </section>
+      <section>
+         <h3>Ingredients</h3>
+         <ul>
+            <LiComponent v-for="ingredient of ingredientsList" :key="ingredient" :content="ingredient" />
+         </ul>
+      </section>
+   </main>
+</template>
+
+<script>
+import LiComponent from './LiComponent.vue'
+
+export default {
+   name: 'DishComponent',
+   components: {
+      LiComponent
+   },
+   data(){
+      return{
+         ingredientsStr: "8 small Granny Smith apples, dsafasdfadfa or as needed &#44;  ½ cup unsalted butter &#44; 3 tablespoons all-purpose flour &#44; ½ cup white sugar &#44;  ½ cup packed brown sugar &#44; ¼ cup water&#44; 1 (9 inch) doub",
+         ingredientsList: []
+      }
+   },
+   methods:{
+      createList(){
+         // this.ingredientsStr = this.ingredientsStr.replaceAll("&#44;", "//");
+         this.ingredientsList = this.ingredientsStr.split("&#44;");
+      }
+   },
+   created(){
+      this.createList();
+   }
+}
+</script>
+
+<style>
+   @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css");
+
+   
+</style>
---
+++ src/components/LiComponent.vue
@@ -0,0 +1,64 @@
+<template>
+   <li class="list" @click="selection" :class="{selected: isSelected}">
+      <section>
+         <i class="fa-solid fa-check"></i>
+      </section>
+      <p>{{content}}</p>
+   </li>
+</template>
+
+<script>
+export default {
+   name: 'LiComponent',
+   props: {
+      content: String,
+   },
+   data(){
+      return{
+         isSelected: false,
+      }
+   },
+   methods:{
+      selection(){
+         this.isSelected = !this.isSelected;
+      }
+   }
+}
+</script>
+
+<style>
+   .list{
+      display: flex;
+      align-items: center;
+      column-gap: 3vw;
+   }
+
+   .list i{
+      display: none;
+   }
+
+   .list > section {
+      width: 3vh;
+      height: 3vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 20px;
+      border: 1px solid black;
+      font-size: 12px;
+   }
+
+   .selected > section{
+      border: 1px solid green;
+      color: whitesmoke;
+      background-color: green;
+   }
+
+   .selected i{
+      display: block;
+   }
+
+   .selected > p{
+      text-decoration: line-through;
+   }
+</style>
