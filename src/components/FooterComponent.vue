fb96a5fdfe195fb03e5adc7fa170d63a4bb15fe2
Jacques Ehrardt <ehrardt38@gmail.com>
2023-06-27 10:54:23 -0700

    FooterComponent: Add template and script

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,31 @@
+<template>
+   <footer>
+      <section>
+         <a href="./" class="logo">
+            <img src="../assets/Dish_dig_logo.png" alt="Dish Dig Logo">
+         </a>
+         <nav class="footerNav">
+            <ul>
+               <li>
+                  <a href="">Home</a>
+               </li>
+               <li>
+                  <a href="">Profile</a>
+               </li>
+            </ul>
+            <a href="#" class="goTop">
+               &#x2191;
+            </a>
+         </nav>
+      </section>
+      <p>
+         &#xa9; All Rights Reserved 2023
+      </p>
+   </footer>
+</template>
+
+<script>
+export default {
+   name: 'FooterComponent'
+}
+</script>
