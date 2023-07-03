ed3996463c86bafe4ccc1981c1fe59e06582d398
Letícia.guerra <84543651+LeticiaMatosGuerra@users.noreply.github.com>
2023-07-03 14:19:17 -0700

    profile 1.0

---
+++ src/components/ProfileComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <figure>
+        <!-- Profile Pic -->
+        <img src="" alt="">
+
+        <figcaption>
+            <!-- Name -->
+            <h3>  </h3> 
+            
+            <!-- Email -->
+            <span> </span>
+
+        </figcaption>
+    </figure>
+</template>
