4b223b2f15d9843f64635a7719c67d6626de3bfc
Letícia.guerra <84543651+LeticiaMatosGuerra@users.noreply.github.com>
2023-07-04 18:21:06 -0700

    ProfilePage - connection with the server - 1.0

--- src/components/ProfileComponent.vue
+++ src/components/ProfileComponent.vue
@@ -1,14 +1,14 @@
 <template>
     <figure>
         <!-- Profile Pic -->
-        <img src="" alt="">
+        <img :src="userInfo.image" :alt="userInfo.name">
 
         <figcaption>
             <!-- Name -->
-            <h3>  </h3> 
+            <h3> {{ userInfo.name }} </h3> 
             
             <!-- Email -->
-            <span> </span>
+            <span> {{ userInfo.email }} </span>
 
         </figcaption>
     </figure>
@@ -16,7 +16,46 @@
 
 <script>
 
-export default ({
+export default {
     name: "ProfileComponent",
-})
+    data() {
+        return {
+            userInfo: {
+                name: "",
+                email: "",
+                image: "",
+            }
+        };
+    },
+
+    methods: {
+        getUserInfo() {
+            // new http request
+            const x = new XMLHttpRequest();
+            // open the new http request with the method get and pass as async (true)
+            x.open('GET', 'http://localhost/FinalProject/api/server.php', true);
+            // onreadysatechange (event) is gonna be called every time theres a change in the request status
+            x.onreadystatechange = () => {
+
+                // 4  is done and 200 is OK
+                if (x.readyState === 4 && x.status === 200) { 
+                    // transform the response in text format
+                    const response = JSON.parse(x.responseText);
+                    
+                    // get the resonse of the request
+                    this.userInfo.name = response.session.username;
+                    this.userInfo.email = response.session.email;
+                    this.userInfo.image = response.session.image;
+
+                }
+            };
+            x.send();
+        }
+    },
+
+    mounted() {
+        this.getUserInfo();
+    }
+
+}
 </script>
